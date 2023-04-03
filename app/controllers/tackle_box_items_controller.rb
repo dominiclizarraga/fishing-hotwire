48712f450d92182648c6f8c775cc4929a9bda44b
dominic lizarraga <dominiclizarraga@hotmail.com>
2023-04-02 17:49:13 -0700

    add data-turbo-frame attribute

--- app/controllers/tackle_box_items_controller.rb
+++ app/controllers/tackle_box_items_controller.rb
@@ -27,14 +27,23 @@ class TackleBoxItemsController < ApplicationController
     @bait = Bait.find(params[:bait_id])
     @item = current_user.tackle_box_items.create!(bait: @bait)
 
-    redirect_to baits_url
+    # redirect_to baits_url by removing this we prevent from another HTTP request (index) Bait.search(params)
+
+    @bait.my_tackle_box_item = @item # we need to do this manually, normally the apth would be invoking method: assig_my_tackle_box
+    
+    render @bait # instead of render partial: "baits/bait", object: @bait
   end
 
   def destroy
     @item = current_user.tackle_box_items.find(params[:id])
     @item.destroy
 
-    redirect_to baits_url
+    # redirect_to baits_url by removing this we prevent from another HTTP request (index) Bait.search(params)
+
+    @bait = @item.bait
+
+    render @bait # instead of render partial: "baits/bait", object: @bait
+
   end
 
 end
